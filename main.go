1fd839cf410149c438a4213bbcce8918c812c962
Guillaume Bour <gbour@online.net>
2019-05-20 14:39:38 +0200

    Fix topic parsing
    Bridge metric looks like '$SYS/broker/connection/BROKER.BRIDGE/state',
    while '.' is not a valid character for prometheus metrics name.
    This commit replace '.' by '_'

--- main.go
+++ main.go
@@ -238,6 +238,7 @@ func parseTopic(topic string) string {
 	name = strings.Replace(name, "/", "_", -1)
 	name = strings.Replace(name, " ", "_", -1)
 	name = strings.Replace(name, "-", "_", -1)
+	name = strings.Replace(name, ".", "_", -1)
 	return name
 }
 
